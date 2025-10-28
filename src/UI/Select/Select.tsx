a86bb7d19b5ca891624da36a29c1c42d6edaaafd
executed <execute.eee@yandex.ru>
2025-10-28 19:34:14 +0300

    add accessibility and functionality to Select

--- src/UI/Select/Select.tsx
+++ src/UI/Select/Select.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useRef, useState, type KeyboardEvent } from 'react';
 
 interface IEvent<T> {
   value: T;
@@ -13,47 +13,94 @@ interface IProps<T> {
   defaultValue: string;
   options: IOptions<T>[];
   onChange?: (arg: IEvent<T>) => void;
+  className?: string;
+  label?: string;
+  id?: string;
 }
 
-const Select = <T,>({ defaultValue, options, onChange }: IProps<T>) => {
+const Select = <T,>({
+  defaultValue,
+  options,
+  onChange,
+  className,
+  label = 'Select option',
+  id = 'select-' + Math.random().toString(36).substring(2, 9),
+}: IProps<T>) => {
   const [isOpened, setOpened] = useState(false);
   const [value, setValue] = useState(defaultValue);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const close = useCallback(() => {
+    setOpened(false);
+    buttonRef.current?.focus();
+  }, []);
 
   return (
-    <div className="relative">
+    <div
+      className={`relative flex items-center ${className}`}
+      onBlur={(e) => {
+        if (!e.relatedTarget?.hasAttribute('data-not-blur')) {
+          setOpened(false);
+        }
+      }}
+      onKeyDown={(e: KeyboardEvent<HTMLDivElement>) => {
+        switch (e.code) {
+          case 'Escape':
+            close();
+            break;
+        }
+      }}>
+      <label id={`${id}-label`} htmlFor={`${id}-button`} className="sr-only">
+        {label}
+      </label>
+
       <button
+        id={`${id}-button`}
+        ref={buttonRef}
         onClick={() => setOpened((state) => !state)}
-        className="flex justify-between items-center gap-x-0.5 cursor-pointer px-1 focus:outline-0 focus:ring focus:ring-blue-500">
+        className="flex justify-between items-center gap-x-0.5 cursor-pointer px-1"
+        aria-haspopup="listbox"
+        aria-expanded={isOpened}
+        aria-labelledby={`${id}-label ${id}-button`}>
         {value}
         <svg
           width={20}
           height={20}
-          className="flex justify-center items-center pointer-events-none"
+          className={`flex justify-center items-center pointer-events-none transition-transform ${
+            isOpened ? 'rotate-180' : ''
+          }`}
           viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true">
           <path
             fillRule="evenodd"
             clipRule="evenodd"
             d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z"
-            fill="white"
+            fill="currentColor"
           />
         </svg>
       </button>
       <ul
-        className={`absolute z-10 bg-black w-full divide-y border-gray rounded-sm transition-opacity ${
-          isOpened ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
-        }}`}>
+        className={`absolute z-10 top-full overflow-y-auto [scrollbar-width:none] bg-black w-full divide-y border-gray rounded-sm transition-[max-height] duration-300 ${
+          isOpened ? 'max-h-[150px]' : 'max-h-0'
+        }`}
+        role="listbox"
+        aria-labelledby={`${id}-label`}
+        tabIndex={-1}>
         {options.map((item, index) => {
           return (
-            <li
-              className="cursor-pointer first:rounded-t-sm last:rounded-b-sm border-gray px-1 hover:bg-gray-900 transition-colors"
-              onClick={() => {
-                setValue(item.text);
-                onChange?.({ value: item.value });
-                setOpened(false);
-              }}
-              key={index}>
-              {item.text}
+            <li role="option" key={index}>
+              <button
+                data-not-blur
+                onClick={() => {
+                  setValue(item.text);
+                  onChange?.({ value: item.value });
+                  close();
+                }}
+                tabIndex={isOpened ? 0 : -1}
+                className="w-full flex justify-start focus:outline-0 focus:bg-gray-900 cursor-pointer first:rounded-t-sm last:rounded-b-sm border-gray px-1 hover:bg-gray-900 transition-colors">
+                {item.text}
+              </button>
             </li>
           );
         })}
--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -2,12 +2,14 @@ import { useTranslation } from 'react-i18next';
 import logo from './images/logo.svg';
 import Select from '@/UI/Select/Select';
 import { LANGUAGES_OPTIONS } from '@/constants';
+import SteamIcon from './icons/SteamIcon';
+import XboxIcon from './icons/XboxIcon';
 
 const Header = () => {
   const { t, i18n } = useTranslation();
 
   return (
-    <header className="top-0 left-0 right-0 font-accent text-white absolute text-[16px]">
+    <header className="top-0 left-0 right-0 font-accent font-bold tracking-[10%] text-white absolute text-[16px]">
       <div className="c flex justify-between items-center">
         <a href="#" aria-current="page" aria-label={t('header.logoAlt')}>
           <img
@@ -22,8 +24,8 @@ const Header = () => {
         </a>
         <nav>
           <ul className="flex gap-x-5 text-center">
-            <li className="">
-              <a href="#hero" className="hover:text-orange transition-colors">
+            <li className="hover:text-orange transition-colors">
+              <a href="#hero">
                 {t('header.nav.main')}
               </a>
             </li>
@@ -45,7 +47,21 @@ const Header = () => {
           </ul>
         </nav>
         <div className="flex">
-          <Select<string> defaultValue={i18n.language} options={LANGUAGES_OPTIONS} onChange={(e) => i18n.changeLanguage(e.value)} />
+          <Select<string>
+            defaultValue={i18n.language}
+            options={LANGUAGES_OPTIONS}
+            onChange={(e) => i18n.changeLanguage(e.value)}
+            className='mr-[15px]'
+            id='first'
+          />
+          <div className="flex gap-x-[15px] pl-[15px] border-l border-gray">
+            <a href="#" target="_blank" className="hover:text-white text-orange transition-colors">
+              <XboxIcon />
+            </a>
+            <a href="#" target="_blank" className='hover:text-white text-orange transition-colors'>
+              <SteamIcon />
+            </a>
+          </div>
         </div>
       </div>
     </header>
